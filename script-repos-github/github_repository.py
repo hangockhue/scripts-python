7cf94b75e981a5e336461955bf1a8283f732a8be
hangockhue <healwayhappy@gmail.com>
2018-06-02 15:54:41 +0700

    add

---
+++ script-repos-github/github_repository.py
@@ -0,0 +1,19 @@
+import json
+import requests
+import sys
+
+user = sys.argv[1]
+api_content = requests.get("https://api.github.com/users/{}/repos".format(user))
+
+api_json = json.loads(api_content.text)
+if not api_json:
+    print("User khong co respons")
+try:
+    if api_json["message"] == "Not Found":
+        print("User khong ton tai")
+except Exception:
+    for line in api_json:
+        try:
+            print(line['name'])
+        except Exception:
+            pass
