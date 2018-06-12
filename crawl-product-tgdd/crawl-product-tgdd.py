a2bb5f61e7ab5ee223726bf78410a613214a397d
hangockhue <healwayhappy@gmail.com>
2018-06-12 12:16:32 +0700

    update

--- crawl-product-tgdd/crawl-product-tgdd.py
+++ crawl-product-tgdd/crawl-product-tgdd.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import csv
+
 driver = webdriver.Chrome('/home/ngockhue/Downloads/chromedriver')
 driver.get('https://thegioididong.com/dtdd#i:6')
 html = driver.page_source
@@ -14,9 +16,14 @@ def star_mobile(product):
     part_star = len(product.find_all('i', class_='icontgdd-hstar'))
     return one_star + part_star / 2
 
+csv_file = open('product.csv', 'w')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['Product', 'Price', 'Rating'])
 
 for artical in articals:
     for product in artical.find_all('li'):
         print('Product:' + product.a.h3.string)
         print('Price:' + product.div.string)
         print('Rating' + str(star_mobile(product)))
+        csv_writer.writerow(
+            [product.a.h3.string, product.div.string, star_mobile(product)])
